eb2f4b592072b21d55b51cd25b7247faf4915758
Corey Purdy <coreypizzle@gmail.com>
2013-12-16 20:51:01 +0000

    fix_font

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 
 require 'carrierwave'
 
-config.assets.initialize_on_precompile = false
+
 
 
 
@@ -14,6 +14,8 @@ Bundler.require(:default, Rails.env)
 
 module Datalog
   class Application < Rails::Application
+
+    config.assets.initialize_on_precompile = false
 
 
     # Settings in config/environments/* take precedence over those specified here.
