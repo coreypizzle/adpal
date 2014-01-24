f34ebf2c15286ec40bf392fda01261aefaad9cd7
Corey Purdy <coreypizzle@gmail.com>
2014-01-24 22:21:51 +0000

    chrg fix

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,29 +1,3 @@
 class ChargesController < ApplicationController
 
-def new
-
-end
-
-def create
-  # Amount in cents
-  @amount = 75
-
-  customer = Stripe::Customer.create(
-    :email => 'example@stripe.com',
-    :card  => params[:stripeToken]
-  )
-
-  charge = Stripe::Charge.create(
-    :customer    => customer.id,
-    :interval    => "weekly"
-    :amount      => @amount,
-    :description => 'Ad Customer',
-    :currency    => 'gbp'
-  )
-
-rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to charges_path
-end
-
 end
